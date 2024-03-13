f2f5d768fcc731196115e3ebb7b0af55e2051952
higuaifan <higuaifan@higuaifan.com>
2024-03-13 23:01:49 +0800

    🚀 improve copy

--- src/components/ColorItem.vue
+++ src/components/ColorItem.vue
@@ -13,27 +13,28 @@ import { MMessage } from 'shuimo-ui';
 const props = defineProps<{ info: { color: string, name: string } }>();
 const fill = computed(() => props.info.color);
 
-const getContrastColor = (hexColor: string) => {
+const deepColorCheck = (hexColor: string) => {
   // 将十六进制颜色转换为RGB值
   let r = parseInt(hexColor.substr(1, 2), 16);
   let g = parseInt(hexColor.substr(3, 2), 16);
   let b = parseInt(hexColor.substr(5, 2), 16);
 
   //  判断是深色还是浅色
-  return (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? '#31322C' : '#EBEEE8';
+  return (r * 0.299 + g * 0.587 + b * 0.114) > 186;
 };
-const fontColor = computed(() => getContrastColor(props.info.color));
+const isDeepColor = deepColorCheck(props.info.color);
+const fontColor = computed(() => isDeepColor ? '#31322C' : '#EBEEE8');
+const fontHoverColor = computed(() => isDeepColor ? '#151D29' : '#F5F3F2');
 
-// 点击复制颜色
-const copyColor = () => {
+const copyInfo = (color: string, type: string) => {
   const input = document.createElement('input');
-  input.value = props.info.color;
+  input.value = color;
   document.body.appendChild(input);
   input.select();
   document.execCommand('copy');
   document.body.removeChild(input);
   MMessage.success({
-    content: '颜色已复制到剪贴板',
+    content: `${type}已复制到剪贴板`,
     direction: 'top-left',
   });
 };
@@ -41,14 +42,14 @@ const copyColor = () => {
 </script>
 
 <template>
-  <div class="color-item m-cursor-pointer" @click="copyColor">
+  <div class="color-item">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300.09 80.79" class="color-item-svg-svg">
       <use xlink:href="/color-item.svg#color-item"/>
     </svg>
 
     <div class="color-item-inner">
-      <span>{{ info.name }}</span>
-      <span class="inner-color">{{ info.color }}</span>
+      <span class="m-cursor-pointer" @click="copyInfo(info.name,`颜色名称【${info.name}】`)">{{ info.name }}</span>
+      <span class="inner-color m-cursor-pointer" @click="copyInfo(info.color,`颜色【${info.color}】`)">{{ info.color }}</span>
     </div>
 
   </div>
@@ -75,8 +76,12 @@ const copyColor = () => {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  font-weight: bold;
   font-size: 2.4rem;
+
+  span:hover {
+    color: v-bind(fontHoverColor);
+    font-weight: bold;
+  }
 }
 
 .inner-color {
